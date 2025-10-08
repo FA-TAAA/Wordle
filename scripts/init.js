c1b06d0850f30d79dd08f1f297009e8d71f6f24b
FA-TAAA <doncappuccino@protonmail.com>
2025-10-08 22:30:44 +0200

    Fixed indicator not dipslaying part 2

--- scripts/init.js
+++ scripts/init.js
@@ -6,7 +6,7 @@ const backspaceButton = document.querySelector(".backspace-icon");
 const enterButton = document.querySelector(".enter-icon");
 const wordleSettings = document.querySelector(".wordle__modal");
 const wordleSettingsButton = document.querySelector(".wordle__modal__button");
-const dialogIndicator = document.querySelector(".dialog__indicator__button");
+const dialogIndicator = document.querySelector(".dialog__indicator");
 
 let wordle;
 let wordleLength;
@@ -15,17 +15,13 @@ wordleSettings.showModal();
 wordleSettingsButton.addEventListener("click", async () => {
   wordleLength = parseInt(document.querySelector("#wordle__length").value);
   if (wordleLength <= 0 || wordleLength > 7 || isNaN(wordleLength)) {
-    messageIndicator(
-      "dialog",
-      "The Wordle's length should be between 1 and 7 !",
-    );
+    messageIndicator("The Wordle's length should be between 1 and 7 !");
     return;
   }
   try {
     wordle = await generateNewWordle(wordleLength);
     initializeGame();
     wordleSettings.close();
-    console.log("here");
   } catch (err) {
     console.log(err);
   }
